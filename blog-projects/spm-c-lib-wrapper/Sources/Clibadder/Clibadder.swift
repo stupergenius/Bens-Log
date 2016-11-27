ee85a0b64ee24cde56eb95505006612278e38dc4
Benjamin Snider <bsnider@DVA-C02MW3K0FD58.local>
2016-11-26 21:46:02 -0700

    Making structs public.

--- blog-projects/spm-c-lib-wrapper/Sources/Clibadder/Clibadder.swift
+++ blog-projects/spm-c-lib-wrapper/Sources/Clibadder/Clibadder.swift
@@ -4,13 +4,13 @@ protocol Addable {
     func add(_ x: Int, _ y: Int) -> Int
 }
 
-struct SimpleAdd: Addable {
+public struct SimpleAdd: Addable {
     func add(_ x: Int, _ y: Int) -> Int {
         return libadder.add(x, y)
     }
 }
 
-struct StructAdd: Addable {
+public struct StructAdd: Addable {
     func add(_ x: Int, _ y: Int) -> Int {
         let op = add_operation(x: x, y: y, result: 0)
         let result = libadder.added(op)
@@ -18,7 +18,7 @@ struct StructAdd: Addable {
     }
 }
 
-struct PointerAdd: Addable {
+public struct PointerAdd: Addable {
     func add(_ x: Int, _ y: Int) -> Int {
         var op = add_operation(x: x, y: y, result: 0)
         libadder.adding(&op)
