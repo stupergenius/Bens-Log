9d02ccf5795eb6f32f92caeb5bb4ee715cf93488
Benjamin Snider <bsnider@DVA-C02MW3K0FD58.local>
2016-11-26 17:15:11 -0700

    Adding support for markdown templates when generating entries.

--- tools/pelican-template-generator.py
+++ tools/pelican-template-generator.py
@@ -8,13 +8,14 @@ def main():
     now_date_keyword = 'now'
     mtime_date_keyword = 'mtime'
     
-    parser = argparse.ArgumentParser(description='Generates a Pelican blog template reStructuredText file.')
+    parser = argparse.ArgumentParser(description='Generates a Pelican blog template file.')
     parser.add_argument('title')
     parser.add_argument('-f', dest='outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     parser.add_argument('--date', help='Supply a date to override the current date. If nothing or "' + now_date_keyword +'" is passed we will use the current date. If "' + mtime_date_keyword + '" is passed we will not generate a date and Pelican will use the file\'s mtime for the post\'s date.')
     parser.add_argument('--author', help='Passing "' + current_user_keyword + '" to this option will try to get the currently logged in user.')
     parser.add_argument('--category')
     parser.add_argument('--tags', help='Comma delimited list of tags.')
+    parser.add_argument('--format', help='Specify either "md" for markdown (default) or "rst" for reStructuredText.', default='md')
     args = parser.parse_args()
     
     tmpl_date = None
@@ -34,17 +35,29 @@ def main():
         except ImportError:
             import getpass
             tmpl_author = getpass.getuser()
+
+    template = ''
+    if (args.format == 'rst'):
+        tmpl_lines = []
+        tmpl_lines.append(args.title)
+        tmpl_lines.append('=' * len(args.title))
+        if (tmpl_date): tmpl_lines.append(':date: ' + tmpl_date)
+        if (tmpl_author): tmpl_lines.append(':author: ' + tmpl_author)
+        if (args.category): tmpl_lines.append(':category: ' + args.category)
+        if (args.tags): tmpl_lines.append(':tags: ' + args.tags)
+        tmpl_lines.append('')
+        template = "\n".join(tmpl_lines)
+    else:
+        tmpl_lines = []
+        tmpl_lines.append('Title: ' + args.title)
+        if (tmpl_date): tmpl_lines.append('Date: ' + tmpl_date)
+        if (tmpl_author): tmpl_lines.append('Author: ' + tmpl_author)
+        if (args.category): tmpl_lines.append('Category: ' + args.category)
+        if (args.tags): tmpl_lines.append('Tags: ' + args.tags)
+        tmpl_lines.append('')
+        template = "\n".join(tmpl_lines)
     
-    tmpl_lines = []
-    tmpl_lines.append(args.title)
-    tmpl_lines.append('=' * len(args.title))
-    if (tmpl_date): tmpl_lines.append(':date: ' + tmpl_date)
-    if (tmpl_author): tmpl_lines.append(':author: ' + tmpl_author)
-    if (args.category): tmpl_lines.append(':category: ' + args.category)
-    if (args.tags): tmpl_lines.append(':tags: ' + args.tags)
-    tmpl_lines.append('')
-    
-    args.outfile.write("\n".join(tmpl_lines))
+    args.outfile.write(template)
 
 if (__name__ == '__main__'):
     main()
