3a31e0c076b07fe306c65974a07c5695e58cf346
Benjamin Snider <stupergenius@gmail.com>
2022-08-20 22:27:21 -0400

    Copy updates.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,8 +9,14 @@ export default class Bio extends React.Component {
         <ProfilePic />
         <h2 className="author-name">Benjamin Snider</h2>
         <p className="author-bio">
-          Hi! 👋 I'm <strong>Ben</strong> and I like to write about technical and nerdy things. Mostly about Swift and iOS, dabbling in machine learning and open source development.{' '}
-          <br/>
+          Hi! 👋 I'm <strong>Ben</strong> and I like to write about technical and nerdy things.
+          Historically about Swift and iOS.
+          But, I've recently started a masters program in computer science&nbsp;
+          <a href="https://omscs.gatech.edu/" target="_blank" rel="noreferrer noopener">
+            (Georgia Tech's OMSCS)
+          </a>
+          , so the content here may pivot as such.
+          &nbsp;
           <Link to="/contact">Get @me!</Link>
         </p>
       </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const About = () => (
       <ProfilePic />
     </div>
     <MainPageContent>
-      <p>I'm an iOS developer by trade, and otherwise identify as a husband, cyclist, vegan and nerd. Technology is my life, and mobile technology is where I choose to direct my focus. Mobile technology is how the world interacts, and so being at the forefront of this trend is equal parts challenging and rewarding.</p>
+      <p>I'm a Technical Lead (ex iOS developer) by trade, and otherwise identify as a husband, cyclist, vegan and nerd. Technology is my life, and interactive technology is where I choose to direct my focus. Technology is how the world interacts, and so being at the forefront of this trend is equal parts challenging and rewarding.</p>
       <p>I'm not currently looking for work, but I always <Link to="/contact">welcome connections</Link>.</p>
     </MainPageContent>
   </>
